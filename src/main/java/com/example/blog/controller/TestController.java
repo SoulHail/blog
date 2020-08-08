321d9bea7467eb2f401616daf126a992dab5a4b6
codeBear <1050848292@qq.com>
2020-08-08 23:08:55 +0800

    IDEA整合GitHub测试

--- src/main/java/com/example/blog/controller/TestController.java
+++ src/main/java/com/example/blog/controller/TestController.java
@@ -4,14 +4,13 @@ import com.example.blog.api.TestApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @Slf4j
 @RestController
 public class TestController implements TestApi {
 
     @Override
     public String hello() {
-        log.info("======进入hello方法=====");
+        log.info("=======进入hello方法======");
         return "hello";
     }
 }
