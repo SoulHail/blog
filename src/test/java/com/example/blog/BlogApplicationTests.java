fa489c483ac7ef07fa1c5a6f3f9683dfffd0f607
codeBear <1050848292@qq.com>
2020-08-26 14:10:04 +0800

    add测试用例
    add测试用例

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,10 +1,21 @@
 package com.example.blog;
 
+import com.example.blog.entity.ten.Cuboid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogApplicationTests {
+
+    @Test
+    public void test() {
+        Cuboid cuboid = Cuboid.builder()
+                .chaNg("wew")
+                .kuAn("dsds")
+                .gao("dsd")
+                .build();
+        System.out.println(cuboid);
+    }
 
     @Test
     void contextLoads() {
